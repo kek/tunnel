7dae768cb8111337d6f0ccbeec75b1d12394bc5d
Karl Eklund <localpart@gmail.com>
2017-04-10 22:48:09 +0200

    Add two scripts to start epmdless nodes

---
+++ node0.sh
@@ -0,0 +1,1 @@
+iex --erl "-proto_dist epmdless -start_epmd false -epmd_module epmdless_epmd_client -setcookie strängnäs" --name node0@127.0.0.1 -S mix
---
+++ node1.sh
@@ -0,0 +1,1 @@
+iex --erl "-proto_dist epmdless -start_epmd false -epmd_module epmdless_epmd_client -setcookie strängnäs" --name node1@127.0.0.1 -S mix
